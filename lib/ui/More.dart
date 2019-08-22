174ff976287763c0f5b99ea732f78041c037a289
Andrew-Baher <37151807+Andrew-Baher@users.noreply.github.com>
2019-08-22 15:02:02 +0200

    more is missing

--- lib/ui/More.dart
+++ lib/ui/More.dart
@@ -9,7 +9,7 @@ import 'PendingRequests.dart';
 import 'SignIn.dart';
 
 bool hasAuth;
-bool IsSurvey=false;
+bool IsSurvey = false;
 
 class More extends StatefulWidget {
   @override
@@ -19,7 +19,6 @@ class More extends StatefulWidget {
 }
 
 class _MyAppState3 extends State<More> {
-
   Future getData() async {
     await new Future.delayed(const Duration(seconds: 0));
     setState(() {});
@@ -31,207 +30,201 @@ class _MyAppState3 extends State<More> {
     return MaterialApp(
       home: Scaffold(
           body: ListView(
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  mainAppState.openAnotherTab(5);
-                },
-                child: ListTile(
-                  leading: Icon(
-                    Icons.person,
-                    size: MediaQuery.of(context).size.width / 16,
-                  ),
-                  title: Text(
-                    'Profile',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              mainAppState.openAnotherTab(5);
+            },
+            child: ListTile(
+              leading: Icon(
+                Icons.person,
+                size: MediaQuery.of(context).size.width / 16,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: ListTile(
+              leading: Icon(
+                Icons.settings,
+                size: MediaQuery.of(context).size.width / 16,
+              ),
+              title: Text(
+                'Settings',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              if (mainEmployee.employeeAuthority.toString() == "User") {
+                mainAppState.openAnotherTab(8);
+              } else {
+                mainAppState.openAnotherTab(9);
+              }
+            },
+            child: ListTile(
+              leading: Icon(
+                Icons.priority_high,
+                size: MediaQuery.of(context).size.width / 16,
+              ),
+              title: Text(
+                'Complaints',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              mainAppState.openAnotherTab(10);
+            },
+            child: ListTile(
+              leading: Icon(
+                Icons.person_pin,
+                size: MediaQuery.of(context).size.width / 16,
+              ),
+              title: Text(
+                'About us',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: IsSurvey,
+            child: GestureDetector(
+              onTap: () {
+                mainAppState.openAnotherTab(10);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.assignment,
+                  size: MediaQuery.of(context).size.width / 16,
+                ),
+                title: Text(
+                  'Make a survey',
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 20,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              GestureDetector(
-                onTap: () {},
-                child: ListTile(
-                  leading: Icon(
-                    Icons.settings,
-                    size: MediaQuery.of(context).size.width / 16,
-                  ),
-                  title: Text(
-                    'Settings',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+            ),
+          ), // Appear if user is admin
+
+          Visibility(
+            visible: hasAuth,
+            child: GestureDetector(
+              onTap: () {
+                mainAppState.openAnotherTab(3);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.assignment_ind,
+                  size: MediaQuery.of(context).size.width / 16,
+                ),
+                title: Text(
+                  'Pending Requests',
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 20,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              GestureDetector(
-                onTap: () {
-                  if(mainEmployee.employeeAuthority.toString() == "User"){
-                    mainAppState.openAnotherTab(8);
-                  }
-                  else {
-                    mainAppState.openAnotherTab(9);
-                  }
-                },
-                child: ListTile(
-                  leading: Icon(
-                    Icons.priority_high,
-                    size: MediaQuery.of(context).size.width / 16,
-                  ),
-                  title: Text(
-                    'Complaints',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+            ),
+          ), // Appear if user is admin
+
+          Visibility(
+            visible: hasAuth,
+            child: GestureDetector(
+              onTap: () {
+                mainAppState.openAnotherTab(1);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.add_a_photo,
+                  size: MediaQuery.of(context).size.width / 16,
+                ),
+                title: Text(
+                  'Add benefit',
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 20,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {});
-                },
-                child: ListTile(
-                  leading: Icon(
-                    Icons.person_pin,
-                    size: MediaQuery.of(context).size.width / 16,
-                  ),
-                  title: Text(
-                    'About us',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+            ),
+          ), // Appear if user is admin
+
+          Visibility(
+            visible: hasAuth,
+            child: GestureDetector(
+              onTap: () {
+                mainAppState.openAnotherTab(2);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.message,
+                  size: MediaQuery.of(context).size.width / 16,
+                ),
+                title: Text(
+                  'Send a message',
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 20,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              Visibility(
-                visible: IsSurvey,
-                child: GestureDetector(
-                  onTap: () {
-                    mainAppState.openAnotherTab(7);
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.assignment,
-                      size: MediaQuery.of(context).size.width / 16,
-                    ),
+            ),
+          ), // Appear if user is admin
 
-                    title: Text(
-                      'Make a survey',
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width / 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
+          Visibility(
+            visible: hasAuth,
+            child: GestureDetector(
+              onTap: () {
+                mainAppState.openAnotherTab(4);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.announcement,
+                  size: MediaQuery.of(context).size.width / 16,
                 ),
-              ), // Appear if user is admin
-
-              Visibility(
-                visible: hasAuth,
-                child: GestureDetector(
-                  onTap: () {
-                    mainAppState.openAnotherTab(3);
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.assignment_ind,
-                      size: MediaQuery.of(context).size.width / 16,
-                    ),
-
-                    title: Text(
-                      'Pending Requests',
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width / 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ), // Appear if user is admin
-
-              Visibility(
-                visible: hasAuth,
-                child: GestureDetector(
-                  onTap: () {
-                    mainAppState.openAnotherTab(1);
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.add_a_photo,
-                      size: MediaQuery.of(context).size.width / 16,
-                    ),
-
-                    title: Text(
-                      'Add benefit',
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width / 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ), // Appear if user is admin
-
-              Visibility(
-                visible: hasAuth,
-                child: GestureDetector(
-                  onTap: () {
-                    mainAppState.openAnotherTab(2);
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.message,
-                      size: MediaQuery.of(context).size.width / 16,
-                    ),
-
-                    title: Text(
-                      'Send a message',
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width / 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ), // Appear if user is admin
-
-              Visibility(
-                visible: hasAuth,
-                child: GestureDetector(
-                  onTap: () {
-                    mainAppState.openAnotherTab(4);
-                  },
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.announcement,
-                      size: MediaQuery.of(context).size.width / 16,
-                    ),
-
-                    title: Text(
-                      'Add a survey',
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width / 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ), // Appear if user is admin
-
-              GestureDetector(
-                onTap: () {
-                  mainAppState.openAnotherTab(6);
-                },
-                child: ListTile(
-                  leading: Icon(
-                    Icons.power_settings_new,
-                    size: MediaQuery.of(context).size.width / 16,
-                  ),
-                  title: Text(
-                    'Log out',
-                    style: TextStyle(
-                        fontSize: MediaQuery.of(context).size.width / 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+                title: Text(
+                  'Add a survey',
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 20,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-            ],
-          )),
+            ),
+          ), // Appear if user is admin
+
+          GestureDetector(
+            onTap: () {
+              mainAppState.openAnotherTab(6);
+            },
+            child: ListTile(
+              leading: Icon(
+                Icons.power_settings_new,
+                size: MediaQuery.of(context).size.width / 16,
+              ),
+              title: Text(
+                'Log out',
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 
@@ -245,8 +238,6 @@ class _MyAppState3 extends State<More> {
       IsSurvey = dataSnapShot.value;
     });
     hasAuth = false;
-    if(mainEmployee.employeeAuthority == "Manager")
-      hasAuth = true;
+    if (mainEmployee.employeeAuthority == "Manager") hasAuth = true;
   }
-
 }
